e10b5c4a8959ff017f05f8b1c740e7103a5eef92
Satya <89769507+SatyaRy@users.noreply.github.com>
2025-08-30 20:23:42 +0700

    feat: seperate services/export CSV file directly to database

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,53 @@
-import express from "express";
-import cors from "cors"
-import dotenv from "dotenv"
-import {route} from "./routes/routes";
-const app = express()
-dotenv.config()
-const port = 3000
-app.use(cors())
-app.use(express.json())
-app.use(route)
-app.get("/", (req, res) => {
-  res.send("Welcome to library management system");
-});
+import readlineSync from "readline-sync";
+import dotenv from "dotenv";
+import {
+  getBookService,
+} from "./service/filterBook";
+import { PrismaClient, Prisma } from "@prisma/client";
+import { addBookByCSV, addBookService } from "./service/addBook";
+import { updateBookService } from "./service/updateBook";
+import { deleteBookService } from "./service/deleteBook";
+const prisma = new PrismaClient();
+dotenv.config();
+const main = async () => {
+  let choice = 0;
+  while (choice !== 5) {
+    console.log(`
+      _________________________
+        Library Management CLI
+        1. Add Book
+        2. Update Book
+        3. Delete Book
+        4. Filter Books
+        5. Export CSV file directly
+        6. Exist
+      _________________________
+        `);
 
-app.listen(port,()=>{
-    console.log(`App is currently running on port ${port}`)
-})
-export default app;
+    choice = Number(readlineSync.question("Enter your choice: "));
+    switch (choice) {
+      case 1:
+        await addBookService();
+        break;
+      case 2:
+        await updateBookService();
+        break;
+      case 3:
+        await deleteBookService();
+        break;
+      case 4:
+        await getBookService();
+        break;
+      case 5:
+        await addBookByCSV();
+      case 6:
+        console.log("Exiting program. Goodbye!");
+        break;
+      default:
+        console.log("Invalid choice. Try again.");
+    }
+  }
+  await prisma.$disconnect();
+};
+
+main();
